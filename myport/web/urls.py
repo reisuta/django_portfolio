962e2c33b384ff4e5a443e70bedaa092c2a50bc7
amaki <in345vain@gmail.com>
2021-08-03 20:51:33 +0900

    second commit

--- myport/web/urls.py
+++ myport/web/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static # ← これ追加
+from django.conf import settings 
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('<int:write>/',views.detail, name='detail')
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
